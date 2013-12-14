3acef23048b3569b4a37ccb11153cea7f32c4374
Vladimir Kocheryzhkin <vkocherizhkin@gmail.com>
2013-12-14 19:14:18 +0400

    add down migration

--- db/migrate/001_create_questions.rb
+++ db/migrate/001_create_questions.rb
@@ -1,7 +1,10 @@
 class CreateQuestions < ActiveRecord::Migration
-  def change
-    create_table :questions do |t|
+  def self.up
+    create_table :questions do |t|      
       t.string :textQuestion #Таблица Questions с текстовым полем "текст вопроса"
     end
+  end
+  def self.down
+    drop_table :questions
   end
 end
--- db/migrate/003_create_useranswers.rb
+++ db/migrate/003_create_useranswers.rb
@@ -1,9 +1,12 @@
 class CreateUseranswers < ActiveRecord::Migration
-  def change
+  def self.up
     create_table :useranswers do |t|
       t.belongs_to :users #Связь с таблицей Users
       t.belongs_to :answers #Связь с таблицей Answers
     end
+  end
+  def self.down
+    drop_table :useranswers
   end
 end
 # Вид таблицы:
