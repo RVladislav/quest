70a8146851a7d08f3fc98e5ae75a55ada5fd2dcc
RVladislav <Vladislav.Razhnov@gmail.com>
2013-12-15 09:32:49 +0400

    fix

--- init.rb
+++ init.rb
@@ -3,8 +3,8 @@ require 'redmine'
 Redmine::Plugin.register :quest do
   name 'Quest plugin'
   author 'Vladislav R.'
-  description 'Survey plugin'
-  version '0.0.1'
+  description 'Votes'
+  version '1.0.0'
   menu :top_menu, :questions, { :controller => 'questions', :action => 'index' }, :caption => 'Questions'
   #Вывод плагина в главном меню redmine
 end
